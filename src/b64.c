6da6e31f8f0157fae3a0532d8a151fac00e21289
Ankur Seth <iankurseth@gmail.com>
2025-04-23 00:25:16 +0530

    Added help info

--- src/b64.c
+++ src/b64.c
@@ -96,6 +96,34 @@ int base64_decode(FILE *in, FILE *out) {
 }
 
 int main(int argc, char *argv[]) {
+	if (argc == 2) {
+		if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+			printf(
+				"Usage: b64 [options] [file1 file2 ...]\n"
+				"\n"
+				"Base64 encoder/decoder. Reads input files or stdin, writes to a single output or stdout.\n"
+				"\n"
+				"Options:\n"
+				"  -d    --decode       Decode input instead of encoding\n"
+				"  -o <file>            Write output to <file> instead of stdout\n"
+				"  -h    --help         Show this help message and exit\n"
+				"  -v    --version      Show version information and exit\n"
+				"\n"
+				"If no input files are given, input is read from stdin.\n"
+				"\n"
+				"Examples:\n"
+				"  b64 input.txt > out.b64\n"
+				"  b64 -d -o decoded.bin out.b64\n"
+				"  cat data | b64 > encoded.txt\n"
+				"\n"
+			);
+			return 0;
+		} else if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) {
+			printf("%s - %s - %s\n", APP_NAME, VERSION_FULL_STR, COMPILE_INFO);
+			return 0;
+		}
+	}
+
 	int error = 0;
 	bool decode = 0;
 	bool ignore_garbage = 0; // not implemented yet, may not be needed
@@ -110,14 +138,6 @@ int main(int argc, char *argv[]) {
 			decode = true;
 		} else if (strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--ignore-garbage") == 0) {
 			ignore_garbage = true;
-		} else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
-			printf("Help info is yet to be added.\n");
-			free(infiles);
-			return 0;
-		} else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0) {
-			printf("%s - %s - %s\n", APP_NAME, VERSION_FULL_STR, COMPILE_INFO);
-			free(infiles);
-			return 0;
 		} else if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) {
 			out = fopen(argv[++i], decode ? "wb" : "w");
 			if (!out) {
